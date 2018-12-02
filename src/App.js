0cefc4e8cc7e4cda0f00176504971bbe5211c1c1
Maruf Siddiqui <siddiquim@mediamarktsaturn.com>
2018-12-02 15:32:58 +0100

    Add prettierrc and reformat

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Services from "./components/pages/Services";
-import ServicesDetails from "./components/pages/ServicesDetails";
-import Contact from "./components/pages/Contact";
-import Menu from "./components/Menu";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Services from './components/pages/Services';
+import ServicesDetails from './components/pages/ServicesDetails';
+import Contact from './components/pages/Contact';
+import Menu from './components/Menu';
 
 class App extends Component {
   render() {
